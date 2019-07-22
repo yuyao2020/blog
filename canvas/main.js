70619f34be5af8552fd8e5e76ada9ed27409edb6
yuyao2020 <980903093@qq.com>
2019-07-22 13:59:40 +0800

    触屏兼容

--- canvas/main.js
+++ canvas/main.js
@@ -2,88 +2,142 @@ var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
 
 autoSetCanvaSize(yyy)
-listenToMouse(yyy)
+listenToUser(yyy)
 
-  
- var eraserEnabled = false
- eraser.onclick = function(){
- eraserEnabled = true
- actions.className = 'actions x'
+
+var eraserEnabled = false
+eraser.onclick = function () {
+  eraserEnabled = true
+  actions.className = 'actions x'
 }
- 
- brush.onclick = function(){
- eraserEnabled = false
- actions.className = 'actions'
+
+brush.onclick = function () {
+  eraserEnabled = false
+  actions.className = 'actions'
 }
-  
-              /*$$$$$$$$$*/
+
+/*$$$$$$$$$*/
 //监听键盘
-function listenToMouse(canvas){
-  var lastPoint = {x: undefined, y: undefined};
-  canvas.onmousedown = function(aaa){
-  var x = aaa.clientX
-  var y = aaa.clientY
-  using = true
-  if(eraserEnabled){
-    context.clearRect(x,y,10,10)
-  }else{
-    lastPoint = {"x":x,"y":y}
+function listenToUser(canvas) {
+  var using = false
+  var lastPoint = {
+    x: undefined,
+    y: undefined
   }
-}
+  //特性检测
+  if(document.body.ontouchstart !== undefined){
+    //触屏设备
+    canvas.ontouchstart = function (aaa) {
+      console.log('开始摸我了')
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      console.log(x,y)
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
+      }
 
+    }
+    canvas.ontouchmove = function (aaa) {
+      console.log('边摸边动')
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      if (!using) {
+        return
+      }
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+  
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
 
-
-canvas.onmousemove = function(aaa){
-   var x = aaa.clientX
-   var y = aaa.clientY
-   if(!using){return}
-   if(eraserEnabled){
-       context.clearRect(x,y,10,10) 
+    }
+    canvas.ontouchend = function () {
+      using = false;
+      console.log('我好了')
+    }
   }else{
-    
-    var newPoint = {"x":x,"y":y}
-      drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
-    lastPoint = newPoint
-  }
+    //非触屏设备
+    canvas.onmousedown = function (aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+        lastPoint = {
+          "x": x,
+          "y": y
+        }
+      }
+    }
+  
+    canvas.onmousemove = function (mouse) {
+      if (!using) { return }
+      var x = mouse.clientX
+      var y = mouse.clientY
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+  
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+  
+  
+    canvas.onmouseup = function (aaa) {
+      using = false;
+    }
+  } 
+  
 }
-
-
-canvas.onmouseup = function(aaa){
-  using = false;
-}
-}
-
 
 //画线
-function drawLine(x1,y1,x2,y2){
+function drawLine(x1, y1, x2, y2) {
   context.beginPath();
   context.strokeStyle = '#211e55'
-  context.moveTo(x1,y1);//起点
+  context.moveTo(x1, y1); //起点
   context.lineWidth = 5;
-  context.lineTo(x2,y2);//终点
+  context.lineTo(x2, y2); //终点
   context.stroke();
   context.closePath();
 }
 
-function drawCircle(x,y,radius){
+function drawCircle(x, y, radius) {
   context.beginPath()
   context.fillStyle = '#211e55'
-  context.arc(x,y,radius,0,360);
-  context.fill();  
+  context.arc(x, y, radius, 0, 360);
+  context.fill();
 }
 
 //设置画面格式
-  function autoSetCanvaSize(canvas){
+function autoSetCanvaSize(canvas) {
   setCanvasSize()
 
-  window.onresize = function(){
-  setCanvasSize()
+  window.onresize = function () {
+    setCanvasSize()
   }
 
-  function setCanvasSize(){
-  var pageWidth = document.documentElement.clientWidth
-  var pageHeight = document.documentElement.clientHeight
-  canvas.width = pageWidth
-  canvas.height = pageHeight
+  function setCanvasSize() {
+    var pageWidth = document.documentElement.clientWidth
+    var pageHeight = document.documentElement.clientHeight
+    canvas.width = pageWidth
+    canvas.height = pageHeight
   }
-  }
+}
